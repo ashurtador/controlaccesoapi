7a8e72104081b72e449d1e25168ff16c37c5b900
Ana Sofía Hurtado <ashurtador@gmail.com>
2022-12-18 14:49:19 -0500

    Running

--- src/main/java/com/as/controlacceso/ControlaccesoApplication.java
+++ src/main/java/com/as/controlacceso/ControlaccesoApplication.java
@@ -2,9 +2,16 @@ package com.as.controlacceso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ControlaccesoApplication {
+public class ControlaccesoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ControlaccesoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControlaccesoApplication.class, args);
